fbc8349a5ed359b70784075f288bc67e08585694
Paola Calderon <paola.calderon@gorillalogic.com>
2017-07-09 10:40:52 -0600

    Adding validation

--- src/com/example/MyExample.java
+++ src/com/example/MyExample.java
@@ -8,18 +8,42 @@ public class MyExample {
 
     public static void main(String[] args) {
         // write your code here
+        boolean valuesOK = true;
         int limit = Integer.parseInt(args[0]);
 //        ArrayList<Integer> newArray = new ArrayList<Integer>();
-        int newArray[] = new int[limit];
-        for (int i = 0; i < limit; i++) {
-            newArray[i] = Integer.parseInt(args[i + 1]);
-//            newArray.add(Integer.parseInt(args[i + 1]));
-
+        int newArray[] = new int[0];
+        if (isInteger(args[0])) {
+            newArray = new int[limit];
+            for (int i = 0; i < limit; i++) {
+                if (isInteger(args[i + 1])) {
+                    newArray[i] = Integer.parseInt(args[i + 1]);
+                    //newArray.add(Integer.parseInt(args[i + 1]));
+                } else {
+                    valuesOK = false;
+                }
+            }
         }
-        arrayAverage(newArray);
-        System.out.print("Hello World");
+        else{
+            valuesOK = false;
+        }
+        if (!valuesOK){
+            System.out.print("Parameters are not valid");
+        }
+        else{
+            arrayAverage(newArray);
+            System.out.print("Hello World");
+        }
     }
 
+    private static boolean isInteger(String value){
+        boolean isInt = true;
+        try{
+            Integer.valueOf(value);
+        } catch (NumberFormatException e){
+            isInt = false;
+        }
+        return isInt;
+    }
     private static void arrayAverage(int[] myArray) {
         int average = 0;
         for (int i = 0; i < myArray.length; i++) {
